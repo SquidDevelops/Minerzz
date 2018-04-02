a9a5f7e452d7084ce4cee1613e0f38d04ab6afb7
ggM8 <ezekielg@tesqgr.net>
2018-04-02 08:25:25 -0400

    Added some housekeeping classes

--- src/net/TeSqGr/NoahCraft/Entity/Player/Player.java
+++ src/net/TeSqGr/NoahCraft/Entity/Player/Player.java
@@ -6,7 +6,6 @@ import net.TeSqGr.NoahCraft.World.Coordinate;
 public class Player extends Entity {
 
 
-
     public Player(int entityID, Coordinate c){
             super(entityID, c.getX(), c.getY(), c.getZ());
     }
