341b95d9743f08385e15f081118bdec1b3e0a65b
3577856 <jay.ram@verizon.net>
2018-04-04 08:31:50 -0400

    Base of block class

--- src/net/TeSqGr/NoahCraft/World/Block.java
+++ src/net/TeSqGr/NoahCraft/World/Block.java
@@ -1,34 +1,6 @@
 package net.TeSqGr.NoahCraft.World;
 
-public class Block {
-    Block block;
-
-    public BlockTest(Block block) {
-        this.block = block;
-    }
-    public int BlockChooser() {
-        switch (day) {
-            case AIR:
-                return 0;
-                break;
-            case DIRT:
-                return 1;
-                break;
-            case STONE:
-                return 2;
-                break;
-            case GRAVEL:
-                return 3;
-                break;
-            case GRASS:
-                return 4;
-                break;
-            case COBBLESTONE:
-                return 5;
-                break;
-        }
-    }
-}
 public enum Block {
-    AIR, DIRT, STONE, GRAVEL, GRASS, COBBLESTONE
+    AIR, DIRT, GRASS, STONE, COBBLESTONE, WOOD, BEDROCK, WOODPLANKS,
+    SAND, GLASS, WOOL, BRICKS, OBSIDIAN, ICE, SNOW, CLAY
 }
