46ee036fecfc6ebc5211c114152868299ba00de3
3577856 <jay.ram@verizon.net>
2018-04-04 08:55:12 -0400

    Base of block class

--- src/net/TeSqGr/NoahCraft/World/BlockType.java
+++ src/net/TeSqGr/NoahCraft/World/BlockType.java
@@ -4,7 +4,20 @@ public enum BlockType {
 
     AIR(0),
     DIRT(1000),
-    GRASS(1000);
+    GRASS(1000),
+    STONE(1500),
+    COBBLESTONE(1500),
+    WOOD(1000),
+    BEDROCK(-1),
+    WOODPLANKS(1000),
+    SAND(1500),
+    GLASS(1000),
+    WOOL(1000),
+    BRICKS(1500),
+    OBSIDIAN(16000),
+    ICE(1000),
+    SNOW(1000),
+    CLAY(1000);
 
     int breakTime;
 
