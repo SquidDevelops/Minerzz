e348cce113891de982370e514a33b6e83fdf9201
ggM8 <zklgeorge56@gmail.com>
2018-04-10 08:53:50 -0400

    We moving

--- src/net/TeSqGr/NoahCraft/Input/Input.java
+++ src/net/TeSqGr/NoahCraft/Input/Input.java
@@ -2,14 +2,21 @@ package net.TeSqGr.NoahCraft.Input;
 
 import net.TeSqGr.NoahCraft.NoahCraft;
 
-import static org.lwjgl.glfw.GLFW.GLFW_KEY_SPACE;
-import static org.lwjgl.glfw.GLFW.GLFW_KEY_W;
+import static org.lwjgl.glfw.GLFW.*;
 
 public class Input {
 
     public void update() {
         if (KeyboardHandler.isKeyDown(GLFW_KEY_W)) {
-            NoahCraft.instance.renderer.setdZ(-0.1f);
+            NoahCraft.instance.renderer.setdZ(0.5f);
+        } else if (KeyboardHandler.isKeyDown(GLFW_KEY_S)) {
+            NoahCraft.instance.renderer.setdZ(-0.5f);
+        }
+
+        if (KeyboardHandler.isKeyDown(GLFW_KEY_A)) {
+            NoahCraft.instance.renderer.setdX(0.5f);
+        } else if (KeyboardHandler.isKeyDown(GLFW_KEY_D)) {
+            NoahCraft.instance.renderer.setdX(-0.5f);
         }
     }
 
