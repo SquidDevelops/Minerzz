69df67c8484ae96e7ddefc2ce07f556c143c510b
Jason Ramos <jay.ram@verizon.net>
2018-04-26 08:57:09 -0400

    Tree noise generation

--- src/net/TeSqGr/NoahCraft/World/Chunk.java
+++ src/net/TeSqGr/NoahCraft/World/Chunk.java
@@ -1,13 +1,26 @@
 package net.TeSqGr.NoahCraft.World;
+import net.TeSqGr.NoahCraft.World.FastNoise.*;
+
+import java.util.ArrayList;
+
+import static net.TeSqGr.NoahCraft.World.FastNoise.NoiseType.SimplexFractal;
 
 public class Chunk {
 
-    private static Noise n = new Noise();
+    private static Noise n = new Noise(3779999, .001f, SimplexFractal);
+    private static Noise m = new Noise(3779999, .1f, SimplexFractal);
 
     public static int[] genChunk(int xO, int yO){
         int[] heightMap = n.genNoise(xO ,yO);
+        int[] treeMap = m.genNoise(xO, yO);
         int[] chunk = new int[65536];
+        ArrayList<Integer> treeLocations = new ArrayList<>();
         int pos = 0;
+        for (int location : treeMap) {
+            if(location > 0.9f) treeLocations.add(pos);
+            pos++;
+        }
+        pos = 0;
         for (int height : heightMap) {
             for (int i = 0; i < height; i++)
                 if(i > height-2)
@@ -19,13 +32,10 @@ public class Chunk {
                 else
                     chunk[(i*16*16)+((pos/16)*16)+(pos%16)] = BlockType.STONE.blockID;
             pos++;
+
         }
-        /* Test junk
-        for (int height : heightMap) {
-            for (int i = 0; i < height; i++)
-                if(i > height )
-        }
-        */
+
+
         return chunk;
     }
 }
