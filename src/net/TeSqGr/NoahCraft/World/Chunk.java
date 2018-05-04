7fe5924baea7001fb8679c7e8a297a81732d4cf4
ggM8 <zklgeorge56@gmail.com>
2018-05-04 08:50:50 -0400

    m o r e s p e e d

--- src/net/TeSqGr/NoahCraft/World/Chunk.java
+++ src/net/TeSqGr/NoahCraft/World/Chunk.java
@@ -39,20 +39,20 @@ public class Chunk {
         for (int height : heightMap) {
             for (int i = 0; i < height; i++)
                 if (i > height - 2)
-                    chunk[(i * 256) + ((pos >> 4) << 4) + (pos % 16)] = BlockType.GRASS.blockID;
+                    chunk[(i << 8) + ((pos >> 4) << 4) + (pos % 16)] = BlockType.GRASS.blockID;
                 else if (i > height - 6)
-                    chunk[(i * 256) + ((pos >> 4) << 4) + (pos % 16)] = BlockType.DIRT.blockID;
+                    chunk[(i << 8) + ((pos >> 4) << 4) + (pos % 16)] = BlockType.DIRT.blockID;
                 else if (i == 0)
-                    chunk[(i * 256) + ((pos >> 4) << 4) + (pos % 16)] = BlockType.BEDROCK.blockID;
+                    chunk[(i << 8) + ((pos >> 4) << 4) + (pos % 16)] = BlockType.BEDROCK.blockID;
                 else
-                    chunk[(i * 256) + ((pos >> 4) << 4) + (pos % 16)] = BlockType.STONE.blockID;
+                    chunk[(i << 8) + ((pos >> 4) << 4) + (pos % 16)] = BlockType.STONE.blockID;
             pos++;
             //System.out.println(height);
         }
         for (int height : heightMap)
             for (int i = 0; i < height; i++)
                 if (i > height - 1)
-                    chunk[(i * 256) + ((pos >> 4) << 4) + (pos % 16)] = BlockType.WOOD.blockID;
+                    chunk[(i << 8) + ((pos >> 4) << 4) + (pos % 16)] = BlockType.WOOD.blockID;
         pos++;
 
 
