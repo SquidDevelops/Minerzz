79a742a8d39e3e77a90b61636d7cbb0a1837f4c5
ggM8 <zklgeorge56@gmail.com>
2018-05-04 08:29:50 -0400

    Basic Checks for Chunks

--- src/net/TeSqGr/NoahCraft/Entity/Player/Player.java
+++ src/net/TeSqGr/NoahCraft/Entity/Player/Player.java
@@ -2,11 +2,18 @@ package net.TeSqGr.NoahCraft.Entity.Player;
 
 import net.TeSqGr.NoahCraft.Entity.Entity;
 import net.TeSqGr.NoahCraft.Inventory.Inventory;
+import net.TeSqGr.NoahCraft.NoahCraft;
+import net.TeSqGr.NoahCraft.World.Chunk;
 import net.TeSqGr.NoahCraft.World.Coordinate;
 
 public class Player extends Entity {
 
     final float sentFromMyiPhone = 0x1.0p0f;
+
+    private int previousChunkX = 0, previousChunkY = 0, chunkX = 0, chunkY = 0;
+
+
+    private int[] chunk;
 
     private Inventory inventory;
 
@@ -18,6 +25,23 @@ public class Player extends Entity {
 
     public void update() {
         //TODO: Translate the world among the player
+
+
+        if (!(Math.floor(NoahCraft.instance.getCamera().getPosition().x / 16) == chunkX))
+            chunkX = (int) (Math.floor(NoahCraft.instance.getCamera().getPosition().x / 16));
+        if (!(Math.floor(NoahCraft.instance.getCamera().getPosition().y / 16) == chunkY))
+            chunkY = (int) (Math.floor(NoahCraft.instance.getCamera().getPosition().y / 16));
+
+        if (((chunkY != previousChunkY)&& chunkY >= 0) || ((chunkX != previousChunkX) && chunkX >= 0)) {
+            chunk = Chunk.genChunk(chunkX, chunkY);
+            previousChunkX = chunkX;
+            previousChunkY = chunkY;
+        }
+
+
+
+        System.out.println(chunk[1] + " : " + chunkX + " : " + chunkY);
+
 //        System.out.println(toString());
     }
 
